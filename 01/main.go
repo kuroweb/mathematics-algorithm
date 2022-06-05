0a74f0832e4a6af9f7beaf9fb7119e04bbb14b6e
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-06-06 00:25:16 +0900

    [add] 標準入力のテスト

--- 01/main.go
+++ 01/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputs := strings.Split(scanner.Text(), " ")
+	values := []int{}
+
+	for _, i := range inputs {
+		j, _ := strconv.Atoi(i)
+		values = append(values, j)
+	}
+	
+	for _, i := range values {
+		fmt.Println(i * 2)
+	}
+}
