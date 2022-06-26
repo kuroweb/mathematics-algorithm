c9f334653e4b0d9879f8864977c74fe73f1aa869
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-06-26 13:42:36 +0900

    chap3-6-5

---
+++ codes/3-6-5/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+var N int
+var A []int
+
+func main() {
+	// sc := bufio.NewScanner(os.Stdin)
+	// sc.Scan()
+
+	// N, _ := strconv.Atoi(sc.Text())
+	N = 5
+
+	A = make([]int, N + 1)
+	for i := 1; i <= N; i++ {
+		A[i] = i
+	}
+
+	fmt.Println(N, A)
+
+	answer := solve(1, N + 1)
+	fmt.Println(answer)
+}
+
+func solve(l, r int) int {
+	if r - l == 1 { return A[l] }
+
+	m := (l + r) / 2
+	s1 := solve(l, m)
+	s2 := solve(m, r)
+	return s1 + s2
+}
