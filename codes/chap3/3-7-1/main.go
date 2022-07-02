a287f198388739ec4667beffb8972e55cb8faa9d
kuromitsu0104 <kuromitsu0104@gmail.com>
2022-07-02 15:11:13 +0900

    chap3-7-1

--- codes/chap3/3-7-1/main.go
+++ codes/chap3/3-7-1/main.go
@@ -25,13 +25,23 @@ func main() {
 	dp := make([]int, N)
 	for i := 0; i < N; i++ {
 		if i == 0 { dp[i] = 0 }
-		if i == 1 { dp[i] = int(math.Abs(float64(H[i - 1] - H[i]))) }
+		if i == 1 { dp[i] = abs(H[i - 1] - H[i]) }
 		if i >= 2 {
-			v1 := dp[i - 1] + int(math.Abs(float64(H[i - 1] - H[i])))
-			v2 := dp[i - 2] + int(math.Abs(float64(H[i - 2] - H[i])))
-			dp[i] = int(math.Min(float64(v1), float64(v2)))
+			v1 := dp[i - 1] + abs(H[i - 1] - H[i])
+			v2 := dp[i - 2] + abs(H[i - 2] - H[i])
+			dp[i] = min(v1, v2)
 		}
 	}
 
 	fmt.Println("dp: ", dp[N - 1])
+}
+
+func abs(a int) int {
+	abs := math.Abs(float64(a))
+	return int(abs)
+}
+
+func min(a, b int) int {
+	min := math.Min(float64(a), float64(b))
+	return int(min)
 }
